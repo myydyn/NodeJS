ba6af3724074814c0c52fa025e5d472466193728
Duyen <daphne.duyenhuynh@gmail.com>
2026-03-21 21:53:38 +0700

    test husky pre-commit

--- blog/src/index.js
+++ blog/src/index.js
@@ -1,13 +1,13 @@
-const path = require("path");
-const express = require("express");
-const morgan = require("morgan");
-const hbs = require("express-handlebars");
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const hbs = require('express-handlebars');
 const app = express();
 const port = 3000;
 
-const route = require("./routes");
+const route = require('./routes');
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -16,21 +16,17 @@ app.use(express.json());
 
 // template engine
 app.engine(
-  "hbs",
-  hbs.engine({
-    extname: ".hbs",
-  }),
+    'hbs',
+    hbs.engine({
+        extname: '.hbs',
+    }),
 );
-          app.set(
-            "view engine",
-
-            "hbs",
-          );
-app.set("views", path.join(__dirname, "resources", "views"));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources', 'views'));
 
 // route init
 route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Example app listening at http://localhost:${port}`);
 });
